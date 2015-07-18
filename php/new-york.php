cd81037d164708c420fdfe169bd8910ed8fc9092
Flynn Tesoriero <flynntes@users.noreply.github.com>
2015-07-18 20:55:55 +1000

    Adding all the code!

---
+++ php/new-york.php
@@ -0,0 +1,15 @@
+<?php
+$doc = new DOMDocument();
+$doc->load('http://weather.yahooapis.com/forecastrss?p=USNY0996&u=c');
+$channel = $doc->getElementsByTagName("channel");
+foreach($channel as $chnl)
+{
+$item = $chnl->getElementsByTagName("item");
+foreach($item as $itemgotten)
+{
+$describe = $itemgotten->getElementsByTagName("description");
+$description = $describe->item(0)->nodeValue;
+echo "<p style='font-family: Arial, Helvetica, sans-serif; text-align: center;'>".$description."</p>";
+}
+}
+?>
